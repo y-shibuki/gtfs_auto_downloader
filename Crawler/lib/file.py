03051bffe612b5a4acd3829974295dc5bba6a2ff
Hinata Yoshida <h-yoshida@toki.waseda.jp>
2023-05-17 12:09:56 +0900

    構造変更

--- Crawler/lib/file.py
+++ Crawler/lib/file.py
@@ -4,7 +4,7 @@ import os
 
 def get_file_name(operator="富山地鉄", date_type="TripUpdates") -> str:
     date: datetime = datetime.datetime.now()
-    folder_path: str = f"./{operator}/{date_type}/{date.strftime('%Y年%m月%d日')}"
+    folder_path: str = f"./data/{operator}/{date_type}/{date.strftime('%Y年%m月%d日')}"
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
