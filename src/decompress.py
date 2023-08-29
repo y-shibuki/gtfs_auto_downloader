a53d7ffe10d4a5e0a79f11955e165e42800a5e92
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-08-29 10:51:52 +0900

    gzipファイルを使用するように変更

--- src/decompress.py
+++ src/decompress.py
@@ -1,14 +1,14 @@
-import tarfile
 import glob
 import os
+import tarfile
 
 if __name__ == "__main__":
     if not os.path.exists("./data"):
         os.makedirs("./data")
 
-    for path in glob.glob("./zip/*.tar.xz"):
+    for path in glob.glob("./zip/*.tar.gz"):
         try:
-            with tarfile.open(path, 'r:xz') as tar:
+            with tarfile.open(path, 'r:gz') as tar:
                 tar.extractall()
         finally:
             os.remove(path)
